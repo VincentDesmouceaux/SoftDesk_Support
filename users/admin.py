73ebcffaf0536c56c9afd8c9b99931bc92a4950f
VincentDesmouceaux <desmontvincent@gmail.com>
2024-12-11 18:46:59 +0100

    tests de token et différence utilisateurs

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # Configuration des champs affichés dans la liste
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'age', 'can_be_contacted', 'can_data_be_shared')
+    list_filter = ('is_staff', 'is_active', 'can_be_contacted', 'can_data_be_shared')
+    ordering = ('username',)
+
+    # Configuration des champs affichés dans les formulaires de création/modification
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Informations personnelles', {'fields': ('email', 'age', 'can_be_contacted', 'can_data_be_shared')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+        ('Dates importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Champs pour le formulaire de création
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'age', 'is_staff', 'is_active'),
+        }),
+    )
+
+
+# Enregistrement du modèle et de la classe générique dans Django Admin
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
 from .models import CustomUser
 from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_403_FORBIDDEN
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,11 +13,19 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action == 'create':
-            return [AllowAny()]
-        return super().get_permissions()
+            return [AllowAny()]  # Permet la création d'un utilisateur sans authentification
+        elif self.action == 'list':
+            if self.request.user.is_superuser:
+                return [IsAuthenticated()]  # Seuls les super utilisateurs peuvent voir la liste
+            else:
+                self.permission_denied(
+                    self.request,
+                    message="Seuls les administrateurs peuvent accéder à cette ressource."
+                )
+        return [IsAuthenticated()]  # Les autres actions nécessitent une authentification
 
     def perform_create(self, serializer):
         age = serializer.validated_data.get('age')
-        if age < 15:
+        if age and age < 15:
             raise ValidationError("L'utilisateur doit avoir au moins 15 ans pour s'inscrire.")
         serializer.save()
